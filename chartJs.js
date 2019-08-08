4376e026288382eac8aaa07eca98308ac208bd31
nikitasheremet <nikita.sheremet253@gmail.com>
2019-08-08 20:13:54 +0000

    adjusted css code for centering x/y axis labels

--- chartJs.js
+++ chartJs.js
@@ -9,30 +9,29 @@
     xAxisTitle: "x-axis-title",
     yAxisTitle: "y-axis-title",
     valuePosition: "center",
-    barColor: ["grey","lightgreen","lightblue", "purple"],
-    lableColor: "black",
+    barColor: [],
+    labelColor: "black",
     barSpacing: "20px",
-    xAxisLable: ["toronto", "ottawa", "montreal", "kingston", "vancouver", "new york"],
-    type: "stacked"
+    xAxisLabel: []
   };
   // updates default options with options provided by user
   for (let property in options) {
       dOpt[property] = options[property];
   }
-  let arrayLen;
-  let lenData
+  let arrayLen = data.length;
+  let lenData;
   let dataMax;
   //alert(data);
-  
   let dataConv = new Array (data);
 
-  if (dataConv.length === 1) {
+  if (arrayLen === dataConv.length) {
     arrayLen = dataConv.length;
     lenData = data.length;
     dataMax = Math.max(...data);
     data = dataConv;
     dOpt.barColor = new Array(dOpt.barColor);
   } else {
+ /////////////////////////////////////////////////////////
     arrayLen = data.length;
     lenData = data[0].length;
     dataConv = data;
@@ -46,13 +45,13 @@
       }
       maxArray.push(number);
     }
-    alert("maxArray is: " + maxArray);
+    //alert("maxArray is: " + maxArray);
     dataMax = Math.max(...maxArray);
   }
 
-  alert("lenData is:" + lenData);
-  alert("arrayLen is: " + arrayLen);
-  alert("max is: " + dataMax);
+  //alert("lenData is:" + lenData);
+  //alert("arrayLen is: " + arrayLen);
+  //alert("max is: " + dataMax);
   
   let openDiv = "<div class = ";
   let closeDiv = "</div>";
@@ -69,7 +68,7 @@
     case dataMax > 0:
       roundTop = Math.ceil(dataMax);
   }
-  alert("round top: " + roundTop);
+  //alert("round top: " + roundTop);
   
   //functiion creates 6 divs within parent div matching 'elem' in order to organize titles and graphing area
   $(elem).append(function defineGraphArea(){
@@ -133,6 +132,7 @@
             return output;
           },
           "background-color" : function () {
+            
             return dOpt.barColor[a][i];
           },
           "padding-bottom" : function () {
@@ -202,17 +202,20 @@
       return dOpt.titleFontSize;
     },
     "text-align" : "center",
-    "padding-top" : "25px"
+    "padding-top" : "25px",
+    "font-weight" : "bold"
   });
   $(".x-axis-title").css({
     "grid-area" : "4/3/5/4",
-    "text-align" : "center"
+    "text-align" : "center",
+    "font-weight" : "bold"
   });
   $(".y-axis-title").css({
     "grid-area" : "2/1/3/2",
     "writing-mode": "vertical-lr",
     "transform" : "rotate(180deg)",
-    "text-align" : "center"
+    "text-align" : "center",
+    "font-weight" : "bold"
   });
   $(".x-axis").css({
     "grid-area" : "3/3/4/4",
@@ -224,16 +227,26 @@
     "grid-template-rows" : "1fr",
     "grid-column-gap" : "20px",
     "padding-left" : "10px",
-    "padding-right" : "10px",
-    "text-align" : "center"
+    "padding-right" : "10px"
+    
   });
   $(".x-axis").append(function() {//add values to x axis
     let output = "";
     for (i = 0; i < lenData; i++) {
-      output += openDiv + "x-axis-lable-" + i + ">" + dOpt.xAxisLable[i] + closeDiv;
+      output += openDiv + "x-axis-label-" + i + ">" + dOpt.xAxisLabel[i] + closeDiv;
     }
     return output;
+  
   });
+  for (i = 0; i < lenData; i++) {
+    elemXAxis = ".x-axis-label-" + i;
+    alert(elemXAxis);
+    $(elemXAxis).css({
+      "display" : "flex",
+      "align-items" : "center",
+      "justify-content" : "center"
+    })
+  }
   $(".y-axis").css({
     "grid-area" : "2/2/3/3",
     "display" : "grid",
@@ -244,6 +257,7 @@
     output = "";
     for (i = 1; i <= 5; i++) {
       output += openDiv + "y-axis-" + i + ">" + ((roundTop/5)*i).toFixed(1) + closeDiv;
+      //alert("y axis output: " + output);
     }
     return output;
   })
@@ -254,8 +268,9 @@
         let output = (-i-1) + "/1/" + (-i-2) + "/2";
         return output;
       },
-      "text-align" : "center",
-      "padding" : "50% 0"
+      "display" : "flex",
+      "align-items" : "center",
+      "justify-content" : "center"
     })
   }
   $(".graph-area").css({
